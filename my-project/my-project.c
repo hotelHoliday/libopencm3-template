b888541e8b153c7838991c2a4090e14e18bf9efe
Henry Davies <hdavies.3@icloud.com>
2024-10-30 07:10:08 +0800

    Change template to a blink example.

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,9 +1,29 @@
 #include "api.h"
 #include "api-asm.h"
 
-int main(void) {
-	/* add your own code */
-	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
-	return my_func(rev);
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+static void gpio_setup(void)
+{
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+}
+
+int main(void)
+{
+	int i;
+
+	gpio_setup();
+
+	/* Blink the LED (PC8) on the board. */
+	while (1){
+		gpio_toggle(GPIOA, GPIO5);	/* LED on/off */
+		for (i = 0; i < 10000000; i++) {	/* Wait a bit. */
+			__asm__("nop");
+		}
+	}
+
+	return 0;
 }
